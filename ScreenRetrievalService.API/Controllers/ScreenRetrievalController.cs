d32cf344a82cb0bb7a40d58a2b60c935bc82e93f
Ahtyamov <Ahtyamov@WIN-SFUQ8BAFB06>
2022-10-05 17:23:46 +0300

    Добавьте файлы проекта.

---
+++ ScreenRetrievalService.API/Controllers/ScreenRetrievalController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScreenRetrievalService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScreenRetrievalController : ControllerBase
+    {
+        public static byte[] DataImage { get; set; }
+
+        [HttpPost("PictureReception")] 
+        public void PictureReception( ImagePost  bytes)
+        {
+            if(bytes != null)   
+                DataImage = bytes.Bytes;
+        }
+
+        [HttpGet ("GetPicture")]
+        public  ImagePost GetBytes ()
+        {
+            if (DataImage != null)
+                return new ImagePost() { Bytes = DataImage };
+            else
+                return new ImagePost(); 
+        }
+    }
+
+    public class ImagePost
+    {
+        public byte[] Bytes { get; set; }
+    }
+}
